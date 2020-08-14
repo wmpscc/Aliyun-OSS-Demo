9d12a5ddba29df1f12674281a0ac8c65a2b5d601
wmpscc <1214490813@qq.com>
2020-08-14 15:16:36 +0800

    init

---
+++ app/src/main/java/net/ishero/alc_oss/MainActivity.java
@@ -0,0 +1,175 @@
+package net.ishero.alc_oss;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.alibaba.sdk.android.oss.ClientConfiguration;
+import com.alibaba.sdk.android.oss.ClientException;
+import com.alibaba.sdk.android.oss.OSS;
+import com.alibaba.sdk.android.oss.OSSClient;
+import com.alibaba.sdk.android.oss.ServiceException;
+import com.alibaba.sdk.android.oss.callback.OSSCompletedCallback;
+import com.alibaba.sdk.android.oss.callback.OSSProgressCallback;
+import com.alibaba.sdk.android.oss.common.OSSLog;
+import com.alibaba.sdk.android.oss.common.auth.OSSAuthCredentialsProvider;
+import com.alibaba.sdk.android.oss.common.auth.OSSCredentialProvider;
+import com.alibaba.sdk.android.oss.internal.OSSAsyncTask;
+import com.alibaba.sdk.android.oss.model.GetObjectRequest;
+import com.alibaba.sdk.android.oss.model.GetObjectResult;
+import com.alibaba.sdk.android.oss.model.PutObjectRequest;
+import com.alibaba.sdk.android.oss.model.PutObjectResult;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static OSS oss;
+    private static Button btUpload;
+    private static Button btDownload;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        initView();
+        initOSS();
+    }
+
+    private void initView() {
+        PermisionUtils.verifyStoragePermissions(this);
+        btUpload = findViewById(R.id.bt_upload);
+        btUpload.setOnClickListener(this);
+
+        btDownload = findViewById(R.id.bt_download);
+        btDownload.setOnClickListener(this);
+    }
+
+    @SuppressLint("NewApi")
+    private void initOSS() {
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+
+        String stsServer = "http://ishero.net:5000/auth/sts";
+// 推荐使用OSSAuthCredentialsProvider。token过期可以及时更新。
+        OSSCredentialProvider credentialProvider = new OSSAuthCredentialsProvider(stsServer);
+
+// 配置类如果不设置，会有默认配置。
+        ClientConfiguration conf = new ClientConfiguration();
+        conf.setConnectionTimeout(15 * 1000); // 连接超时，默认15秒。
+        conf.setSocketTimeout(15 * 1000); // socket超时，默认15秒。
+        conf.setMaxConcurrentRequest(5); // 最大并发请求数，默认5个。
+        conf.setMaxErrorRetry(2); // 失败后最大重试次数，默认2次。
+
+        oss = new OSSClient(getApplicationContext(), endpoint, credentialProvider);
+
+        OSSLog.enableLog();
+    }
+
+    private void upload() {
+        // 构造上传请求。
+        PutObjectRequest put = new PutObjectRequest("road-oss", "test.mp4",
+                "/storage/emulated/0/DCIM/Camera/VID_20191215_150522.mp4");
+
+// 异步上传时可以设置进度回调。
+        put.setProgressCallback(new OSSProgressCallback<PutObjectRequest>() {
+            @Override
+            public void onProgress(PutObjectRequest request, long currentSize, long totalSize) {
+                Log.d("PutObject", "currentSize: " + currentSize + " totalSize: " + totalSize);
+            }
+        });
+
+        OSSAsyncTask task = oss.asyncPutObject(put, new OSSCompletedCallback<PutObjectRequest, PutObjectResult>() {
+            @Override
+            public void onSuccess(PutObjectRequest request, PutObjectResult result) {
+                Log.d("PutObject", "UploadSuccess");
+                Log.d("ETag", result.getETag());
+                Log.d("RequestId", result.getRequestId());
+            }
+
+            @Override
+            public void onFailure(PutObjectRequest request, ClientException clientExcepion, ServiceException serviceException) {
+                // 请求异常。
+                if (clientExcepion != null) {
+                    // 本地异常，如网络异常等。
+                    clientExcepion.printStackTrace();
+                }
+                if (serviceException != null) {
+                    // 服务异常。
+                    Log.e("ErrorCode", serviceException.getErrorCode());
+                    Log.e("RequestId", serviceException.getRequestId());
+                    Log.e("HostId", serviceException.getHostId());
+                    Log.e("RawMessage", serviceException.getRawMessage());
+                }
+            }
+        });
+// task.cancel(); // 可以取消任务。
+// task.waitUntilFinished(); // 等待上传完成。
+    }
+
+    private void download() {
+        GetObjectRequest get = new GetObjectRequest("road-oss", "test.mp4");
+//设置下载进度回调
+        get.setProgressListener(new OSSProgressCallback<GetObjectRequest>() {
+            @Override
+            public void onProgress(GetObjectRequest request, long currentSize, long totalSize) {
+                OSSLog.logDebug("getobj_progress: " + currentSize + "  total_size: " + totalSize, false);
+            }
+        });
+        OSSAsyncTask task = oss.asyncGetObject(get, new OSSCompletedCallback<GetObjectRequest, GetObjectResult>() {
+            @Override
+            public void onSuccess(GetObjectRequest request, GetObjectResult result) {
+                // 请求成功
+                InputStream inputStream = result.getObjectContent();
+
+                byte[] buffer = new byte[2048];
+                int len;
+
+                try {
+                    while ((len = inputStream.read(buffer)) != -1) {
+                        // 处理下载的数据
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(GetObjectRequest request, ClientException clientExcepion, ServiceException serviceException) {
+                // 请求异常
+                if (clientExcepion != null) {
+                    // 本地异常如网络异常等
+                    clientExcepion.printStackTrace();
+                }
+                if (serviceException != null) {
+                    // 服务异常
+                    Log.e("ErrorCode", serviceException.getErrorCode());
+                    Log.e("RequestId", serviceException.getRequestId());
+                    Log.e("HostId", serviceException.getHostId());
+                    Log.e("RawMessage", serviceException.getRawMessage());
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_upload:
+                upload();
+                break;
+            case R.id.bt_download:
+                download();
+                break;
+            default:
+                break;
+
+        }
+    }
+}
